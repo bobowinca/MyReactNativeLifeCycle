d042d75fb8a9a96d8c940536a0e8a40186254564
bobowinca <bobowinca@gmail.com>
2019-11-08 19:32:09 -0500

    Complete Life Cycle with life cycle methods

--- App.js
+++ App.js
@@ -1,79 +1,12 @@
 /*Example of Reac Native Life Cycle*/
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import React from 'react';
+import {View} from 'react-native';
+import CustomComponent from './src/CustomComponent';
 
-class CustomComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isFetching: false,
-      users: [],
-      counter: 0,
-    };
+const App = () => (
+  <View style={{flex: 1, justifyContent: 'center'}}>
+    <CustomComponent name="C" />
+  </View>
+);
 
-    console.log('Constructor Called.');
-  }
-
-  UNSAFE_componentWillMount() {
-    console.log('componentWillMount called.');
-  }
-
-  increase = () => {
-    const current = this.state.counter;
-    this.setState({coounter: current + 1});
-  };
-
-  // fetchSomethingAPI = () => {
-  //   const current = this.state.counter;
-  //   this.setState({coounter: current + 1});
-  // };
-
-  componentDidMount() {
-    this.increase();
-    this.timer = setInterval(() => this.increase(), 3000);
-    console.log('componentDidMount called.');
-  }
-
-  UNSAFE_componentWillReceiveProps(nextProp) {
-    console.log('componentWillReceiveProps called.', nextProp);
-  }
-
-  shouldComponentUpdate(nextProp, nextState) {
-    console.log('shouldComponentUpdate called.');
-    return true;
-  }
-
-  UNSAFE_componentWillUpdate(nextProp, nextState) {
-    console.log('componentWillUpdate called.');
-  }
-
-  componentDidUpdate(prevProp, prevState) {
-    console.log('componentDidUpdate called.');
-  }
-
-  componentWillUnmount() {
-    console.log('componentWillUnmount called.');
-  }
-
-  componentDidCatch(error, info) {
-    console.log('componentDidCatch called.');
-  }
-
-  render() {
-    return (
-      <View style={{justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Language is: {this.props.name}</Text>
-      </View>
-    );
-  }
-}
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={{flex: 1, justifyContent: 'center'}}>
-        <CustomComponent name="C" />
-      </View>
-    );
-  }
-}
+export default App;
